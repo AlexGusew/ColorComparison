a66359717472fb051a77901d522ea0e209664dcb
gusev <sasha19755791@gmail.com>
2019-02-13 02:12:49 +0300

    Updates

---
+++ precache-manifest.1f401fcb06f25da9e2a095d9ee1e16a2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "86caf163f797300a6e04",
+    "url": "/faustAbc/ColorComparison/settings/static/js/runtime~main.86caf163.js"
+  },
+  {
+    "revision": "53da340e07ec9defa214",
+    "url": "/faustAbc/ColorComparison/settings/static/js/main.53da340e.chunk.js"
+  },
+  {
+    "revision": "ef7d5d49365947d15577",
+    "url": "/faustAbc/ColorComparison/settings/static/js/2.ef7d5d49.chunk.js"
+  },
+  {
+    "revision": "acbb56bf7c220da611f3acbe334e6721",
+    "url": "/faustAbc/ColorComparison/settings/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fad04091111fba0131fa4013cfc69d6c.js"
+  "/faustAbc/ColorComparison/settings/precache-manifest.1f401fcb06f25da9e2a095d9ee1e16a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/faustAbc/ColorComparison/settings/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
