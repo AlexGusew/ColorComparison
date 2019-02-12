2c7c041eaffc1f25e722a57ca19da60325693051
gusev <sasha19755791@gmail.com>
2019-02-13 02:08:54 +0300

    Updates

---
+++ precache-manifest.fad04091111fba0131fa4013cfc69d6c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "53da340e07ec9defa214",
+    "url": "/static/js/main.53da340e.chunk.js"
+  },
+  {
+    "revision": "ef7d5d49365947d15577",
+    "url": "/static/js/2.ef7d5d49.chunk.js"
+  },
+  {
+    "revision": "7a50989deba4db210aa5a023233f04d3",
+    "url": "/index.html"
+  }
+];
