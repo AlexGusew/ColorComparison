964b9064bb32836c982400065478f79656c46d02
gusev <sasha19755791@gmail.com>
2019-02-13 04:28:13 +0300

    Add color picker

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import SMYK from './components/SMYK';
 import RGB from './components/RGB';
 import HLS from './components/HLS';
-import { Grid } from '@material-ui/core';
+import { Grid, Fab } from '@material-ui/core';
 import { types, CMYKtoRGB, RGBtoCMYK, RGBtoHLS, HLStoRGB } from './components/utls';
+import { BlockPicker } from 'react-color';
+import { Colorize } from '@material-ui/icons';
 
 class App extends Component {
   constructor() {
@@ -12,7 +14,13 @@ class App extends Component {
       R: 0,
       G: 150,
       B: 250,
+      isPick: false,
     }
+  };
+
+  handleChangeComplete = (color) => {
+    const { r, g, b } = color.rgb;
+    this.setState({ R: r, G: g, B: b });
   };
 
   commonReturn = {
@@ -53,7 +61,7 @@ class App extends Component {
     switch (type) {
       case types.RGB: {
         const { R: _R, G: _G, B: _B } = data;
-        const addNewValue = { R: _R, G: _G, B: _B, [sliderType]: value };
+        const addNewValue = { R: _R, G: _G, B: _B, [sliderType]: value || e.target.value };
 
         R = addNewValue.R;
         G = addNewValue.G;
@@ -63,7 +71,7 @@ class App extends Component {
       }
       case types.CMYK: {
         const { C: _C, M: _M, Y: _Y, K: _K } = data;
-        const addNewValue = { C: _C, M: _M, Y: _Y, K: _K, [sliderType]: value };
+        const addNewValue = { C: _C, M: _M, Y: _Y, K: _K, [sliderType]: value || e.target.value };
         const { R: _R, G: _G, B: _B } = CMYKtoRGB(addNewValue.C, addNewValue.M, addNewValue.Y, addNewValue.K);
 
         R = _R;
@@ -74,7 +82,7 @@ class App extends Component {
       }
       case types.HLS: {
         const { H: _H, L: _L, S: _S } = data;
-        const addNewValue = { H: _H, L: _L, S: _S, [sliderType]: value };
+        const addNewValue = { H: _H, L: _L, S: _S, [sliderType]: value || e.target.value };
         const { R: _R, G: _G, B: _B } = HLStoRGB(addNewValue.H, addNewValue.L, addNewValue.S);
 
         R = _R;
@@ -90,11 +98,29 @@ class App extends Component {
     this.setState({ R, G, B });
   }
 
+  appearPicker = () => {
+    this.setState({ isPick: !this.state.isPick });
+  }
+
   render() {
+    const { R, G, B, isPick } = this.state;
+
     return (
       <Grid container style={{ height: '100vh' }} justify="center" alignItems="center">
         <Grid item>
           <Grid container justify="center">
+            <Grid item>
+              <Grid container direction="column" justify="flex-start" alignItems="center">
+                <Fab onClick={this.appearPicker} style={{ marginTop: 20 }}>
+                  <Colorize />
+                </Fab>
+                {isPick && <div style={{ position: 'absolute', marginTop: 100 }}>
+                  <BlockPicker
+                    color={{ r: R, g: G, b: B }}
+                    onChangeComplete={this.handleChangeComplete}
+                  /> </div>}
+              </Grid>
+            </Grid>
             <SMYK {...this.toSMYK()} />
             <RGB {...this.toRGB()} />
             <HLS {...this.toHLS()} />
--- src/components/SliderColor/index.js
+++ src/components/SliderColor/index.js
@@ -2,21 +2,38 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/lab/Slider';
 import Grid from '@material-ui/core/Grid';
+import { TextField } from '@material-ui/core';
 
-const SliderColor = ({ value, text, handleChange, min = 0, max = 100 }) => (
-  <Grid>
-    <Typography gutterBottom variant="subtitle2" component="h2">
-      {text}
-    </Typography>
-    <Slider
-      style={{ padding: '12px 0px' }}
-      value={value}
-      aria-labelledby="label"
-      onChange={handleChange(text)}
-      min={min}
-      max={max}
-    />
-  </Grid>
-);
+const SliderColor = ({ value, text, handleChange, min = 0, max = 100 }) => {
+  const isError = () => value < min || value > max;
+
+  return (
+    <Grid>
+      <Typography gutterBottom variant="subtitle2" component="h2">
+        {text}
+      </Typography>
+      <Grid container justify='center' alignItems='center' wrap='nowrap'>
+        <Slider
+          style={{ padding: '12px 0px' }}
+          value={Math.round(value * 100) / 100}
+          aria-labelledby="label"
+          onChange={handleChange(text)}
+          min={min}
+          max={max}
+        />
+        <TextField
+          value={Math.round(value * 100) / 100}
+          onChange={handleChange(text)}
+          margin="none"
+          style={{ marginLeft: 20 }}
+          type="number"
+          error={isError()}
+          helperText={isError() ? "Invalid value" : ' '}
+          
+        />
+      </Grid>
+    </Grid>
+  );
+}
 
 export default SliderColor;
